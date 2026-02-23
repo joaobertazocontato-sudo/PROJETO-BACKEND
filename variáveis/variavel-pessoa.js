e4cd98e5d91315d40f0c85e22e5ba7d01ff93bbf
joaobertazocontato-sudo <joaobertazo.contato@gmail.com>
2026-02-23 12:37:56 +0000

    criações de variaveis

---
+++ variáveis/variavel-pessoa.js
@@ -0,0 +1,13 @@
+
+
+let nome = 'pericles'
+let sobrenome = 'solza'
+const cpf = "888888888-88"
+const rg = "121212 ssp"
+let idade = 22
+let solteiro = true
+let nota = 7 + 2 
+
+console.log(nome)
+
+console.log(typeof nome)
---
+++ variáveis/variavel-produto.js
@@ -0,0 +1,8 @@
+let nomedoproduto  = "funilDeOleoDeCarro"
+let preco = "34,99"
+let disponivel = true
+let quantidade = 8
+
+console.log(nomedoproduto)
+
+console.log("funilDeOleoDeCarro: " + nomedoproduto)

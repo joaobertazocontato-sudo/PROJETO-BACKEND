40af3d49adff62fc877b1702e5455eda96f06df5
joaobertazocontato-sudo <joaobertazo.contato@gmail.com>
2026-03-30 10:58:52 +0000

    function e const

---
+++ funções/funcoes.js
@@ -0,0 +1,10 @@
+function dizOla(){
+    console.log('ola')
+}
+function olapessoa(nome){
+    console.log('ola,' + nome)
+}
+
+dizOla()
+olapessoa('luana')
+olapessoa('mano')
---
+++ funções/funcoes2.js
@@ -0,0 +1,8 @@
+
+function subtracao(n1, n2){
+    const resultado = n1 - n2;
+    console.log('resultado da subtracao:' + resultado)
+}
+
+subtracao(5, 2);
+subtracao(-50, 10)

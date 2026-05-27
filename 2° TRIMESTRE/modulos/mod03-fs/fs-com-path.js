4c986af78855d2981ae39e94b0e64bbdd88ff73a
joaobertazocontato-sudo <joaobertazo.contato@gmail.com>
2026-05-27 11:53:04 +0000

    path

--- 2° TRIMESTRE/modulos/mod03-fs/fs-com-path.js
+++ 2° TRIMESTRE/modulos/mod03-fs/fs-com-path.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 //const caminho = path.join(__dirname, 'README.md')
-//console.log(caminho)
+//console.log(caminho
 const caminho = path.resolve(__dirname, 'README.md')
 const arquivo = fs.readFileSync(caminho, 'utf-8')
 console.log(arquivo)

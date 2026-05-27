df9a64635518403a39b0ac86ef1fae2704e56234
joaobertazocontato-sudo <joaobertazo.contato@gmail.com>
2026-05-27 11:49:38 +0000

    Finalização do módulo FS com path (join e resolve)

---
+++ 2° TRIMESTRE/modulos/mod03-fs/fs-com-path.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+//const caminho = path.join(__dirname, 'README.md')
+//console.log(caminho)
+const caminho = path.resolve(__dirname, 'README.md')
+const arquivo = fs.readFileSync(caminho, 'utf-8')
+console.log(arquivo)
--- 2° TRIMESTRE/modulos/mod03-fs/fs-sem-path.js
+++ 2° TRIMESTRE/modulos/mod03-fs/fs-sem-path.js
@@ -4,5 +4,7 @@ console.log(fs)
 
 //const arquivo = fs.readFileSync('./README.md', 'utf-8');
 
-const arquivofinal = fs.readFileSync(__dirname + '/README.md', 'utf-8');
-console.log(arquivofinal)
+//const caminho = fs.readFileSync(__dirname + '/README.md', 'utf-8');
+console.log(caminho)
+
+

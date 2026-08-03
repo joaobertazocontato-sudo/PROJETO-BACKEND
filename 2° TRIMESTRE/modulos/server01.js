861a839d8dffba586785c10fa536b1264b33e5f4
joaobertazocontato-sudo <joaobertazo.contato@gmail.com>
2026-08-03 11:01:04 +0000

    usando fs com path em server.js

--- 2° TRIMESTRE/modulos/server01.js
+++ 2° TRIMESTRE/modulos/server01.js
@@ -1,4 +1,34 @@
-const server  = require('node:http');
-const port    = 8002
+const http = require('node:http');
+const path = require('node:path');
+const fs = require('node:fs');
 
-const 
+const porta = 8002
+
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
+
+const server = http.createServer((req, res) => {
+    const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
+    const caminhoUrl = novaUrl.pathname
+    if(caminhoUrl === '/'){
+        res.statusCode = '201'
+       //res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(fs.readFileSync(home, 'utf-8')) //chamar o html
+    }
+        if(caminhoUrl === '/'){
+        res.statusCode = '201'
+       //res.setHeader('Content-Type', 'text/plain')
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(fs.readFileSync(sobre, 'utf-8')) //chamar o html
+    } else {
+        res.statusCode = '404'
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h3>404 Página Não Encontrada</h3>')
+    }
+})
+
+server.listen(porta, () => {
+    console.log(`Servidor rodando na porta 
+        http://localhost:${porta}`)
+})
